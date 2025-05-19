0a62d42fca059449072996e116d10bbbf2ef5929
Andreas <andreasbrezo@gmail.com>
2025-05-19 08:00:13 -0300

    minor changes

---
+++ Back_dev/SecurityTask/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use App\Http\Controllers\TestController;
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/', [TestController::class, 'index'])->name('home');
+
+
